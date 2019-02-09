382832569d0961febe55d2e3c7a25e5aa8aed587
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-02-09 10:46:29 -0500

    added lightning exercises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,49 @@
 // Post the taskObject to the database
 
 // Then get all the tasks and print them to the DOM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------- DELETE --------------//
+// Add a delete button to the dom component
+
+// When the user clicks the delete button, get that item's id and delete it from the DB
+
+// Refresh the list
+
+// ---------------- PATCH -------------------------//
+// Add an event listener to the check box
+// When the check box is checked, make a PATCH request to the db to change the completed property from false to true
+// Refresh the list
+
+//------------ PUT ------------------//
+// When the user clicks the edit button, build a form with that item's information
+// add an event listener to the submit button
+// build a new object representing that task
+//PUT it to the database
+// refresh the list

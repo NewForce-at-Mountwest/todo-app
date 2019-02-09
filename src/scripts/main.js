4e0fe7c77a170dca4aa646c58f9fe4932b889d6d
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-02-09 08:03:10 -0500

    added comments for js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
+// Add an event listener to the add button
 
+// When we click on the add button, get the user's input
+
+// Create a new task object
+
+// Post the taskObject to the database
+
+// Then get all the tasks and print them to the DOM
